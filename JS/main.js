c2489c939c762e4faa80f1d07babf927a36c632c
rkmehedi <astermehedi@gmail.com>
2025-03-17 03:45:53 +0600

    addeded active button as well

--- JS/main.js
+++ JS/main.js
@@ -12,7 +12,7 @@
 function loadLesson(){
     fetch("https://openapi.programming-hero.com/api/levels/all")
     .then((res)=>res.json()
-    ).then((mydata) =>showLesson(mydata.data)
+    ).then((mydata) =>{showLesson(mydata.data)}
     );
     }
     
@@ -22,26 +22,55 @@ function loadLesson(){
         const lessonCard= document.getElementById("lesson-card-id");
 
     for (const lesson of data) {
+       
+
         const lessonCardSet=document.createElement("button");
         lessonCardSet.innerHTML=`
         
-        <button onclick="levelGenerator(${lesson.level_no});" class=" lesson-btn"><i class="fa-solid fa-book-open"></i> ${lesson.lessonName}</button>
+        <button id="active-btn-${lesson.level_no}" onclick="levelGenerator(${lesson.level_no});" class=" lesson-btn"><i class="fa-solid fa-book-open"></i> ${lesson.lessonName}</button>
         `;
+        
+        
+        
+        
         lessonCard.append(lessonCardSet);
+        
+        
+        
+        
     }
+   
+    
     }
+    // active class remover
+    function activeRemoval(){
+    const removeActive=document.getElementsByClassName("active-button");
+        for (let active of removeActive){
+           active.classList.remove("active-button");
+        }
+}
+    
     // Level-generator
     function levelGenerator(currentLevel){
         
         // displayHide();
         const url= `https://openapi.programming-hero.com/api/level/${currentLevel}`;
+        // active
+        activeRemoval();
+        
+        const activeButton = document.getElementById(`active-btn-${currentLevel}`);
+            activeButton.classList.add("active-button");
         
         fetch(url)
         .then((res)=>res.json()
         ).then((myLevel) =>
             
-            pronounceGet(myLevel.data)
+          { pronounceGet(myLevel.data)
             
+            
+        } 
+
+        
         );
 
     }
@@ -61,7 +90,7 @@ function loadLesson(){
                     <h3 class="bangla text-[#292524] text-[34px] text-center font-medium">নেক্সট Lesson এ যান।</h3>
                 </div>`;
             }
-
+            
         for (const cardFile of data) {
             const cardSet=document.createElement("div");
 
